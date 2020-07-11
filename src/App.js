c25f494f2b94ec4b115d72954bf5242c2208e3a0
gcgbarbosa <gcgbarbosa@gmail.com>
2020-07-10 19:43:46 -0700

    Added a way to get the svg

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
+import ReactDOM from "react-dom";
 
 import Grid from "@material-ui/core/Grid";
 import Box from "@material-ui/core/Box";
@@ -8,9 +9,147 @@ import TextField from '@material-ui/core/TextField'
 import FormControl from '@material-ui/core/FormControl'
 import Button from '@material-ui/core/Button'
 
-import {TAGComponent} from '@lum-ai/react-tag'
+import {Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle} from '@material-ui/core'
 
+import {TAGComponent} from '@lum-ai/react-tag'
 import CustomParser from './odinson'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {jsonFile: '', jsonMode: true, documentFile: '', backup: '', exportSVG: true}
+  }
+
+  handleTextAreaJsonInput = (json) => {
+    if (this.state.jsonMode) {
+      this.documentFile = json
+      let state = this.state
+      state.jsonFile = JSON.parse(json)
+      this.setState(state)
+    }
+  }
+
+  handleDownloadClick = () => {
+    if (this.state.jsonFile && this.state.jsonMode) {
+      const node = ReactDOM.findDOMNode(this)
+      let state = this.state
+      state.jsonMode = false
+      state.backup = state.documentFile
+      state.documentFile = node.querySelector('.tagContainer').innerHTML
+      this.setState(state)
+    } else if (this.state.jsonMode === false) {
+      let state = this.state
+      state.jsonMode = true
+      state.documentFile = state.backup
+      this.setState(state)
+    }
+
+  }
+
+  handleCloseDownload = () => {
+    var state = this.state
+    state.jsonMode = true
+    this.setState(state)
+  }
+
+  render() {
+    let tag;
+    // only render tag if this is set
+    if (this.state.jsonFile) {
+      tag = (
+        <Grid item xs={12}>
+          <Paper>
+            <TAGVizualization jsonDocument={this.state.jsonFile} exportSVG={this.state.exportSVG} />
+          </Paper>
+        </Grid>
+      )
+    }
+    // 
+    let buttonLabel = 'svgme'
+    if (this.state.jsonMode === false) {
+      buttonLabel = 'backtonormal'
+    }
+    let svgButton;
+    if (this.state.jsonFile) {
+      svgButton = (
+        <Box p={4} pt={0}>
+          <Button onClick={this.handleDownloadClick} color='primary'>
+            {buttonLabel}
+          </Button>
+        </Box>
+      )
+    }
+
+    return (
+      <Grid container direction="column" justify="center" alignItems="center" spacing={4}>
+        <Dialog
+          onClose={this.handleCloseDownload}
+          open={this.open}
+        >
+          <DialogTitle>
+            Something goes here
+          </DialogTitle>
+          <DialogContent>
+            This is the content of the thing.
+            RN it does not really have anything.
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleCloseDownload} color='primary'>
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Grid item xs={12}>
+          <Paper>
+            <Box p={4} minWidth="90vw">
+              <Typography variant="h4" align="center" gutterBottom>
+                Odinson File Editor
+              </Typography>
+            </Box>
+          </Paper>
+        </Grid>
+        {tag}
+        <Grid item xs={12}>
+          <Paper>
+            <TextAreaJson
+              onJsonDocumentChange={this.handleTextAreaJsonInput}
+              documentFile={this.state.documentFile}
+            />
+            {svgButton}
+          </Paper>
+        </Grid>
+
+      </Grid>
+    )
+  }
+}
+
+class TAGVizualization extends React.Component {
+  render() {
+    return (
+      <Box p={4} minWidth="90vw">
+        <Typography variant="h5" align="center" gutterBottom>
+          Tag
+        </Typography>
+        <Box width={1}>
+          <TAGComponent
+            exportSVG={this.props.exportSVG}
+            data={this.props.jsonDocument}
+            topTagCategory="chunk"
+            bottomTagCategory="POS"
+            bottomLinkCategory="universal-enhanced"
+            showTopMainLabel={true}
+            showTopArgLabels={true}
+            customParsers={[{ParserClass: CustomParser, name: 'custom'}]}
+            format="custom"
+          />
+          <Box>
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+}
 
 class TextAreaJson extends React.Component {
   constructor(props) {
@@ -30,6 +169,10 @@ class TextAreaJson extends React.Component {
   }
 
   render() {
+    let print = this.state.jsonDocument
+    if (this.props.documentFile) {
+      print = this.props.documentFile
+    }
     return (
       <Box p={4} minWidth="90vw">
         <form>
@@ -40,85 +183,15 @@ class TextAreaJson extends React.Component {
               width={1}
               variant="outlined"
               onChange={this.handleChange}
-              value={this.state.jsonDocument}
+              value={print}
             />
           </FormControl>
-          <Button onClick={this.handleSubmit} variant="contained">Save</Button>
+          <Button onClick={this.handleSubmit} variant="contained">Plot</Button>
         </form>
       </Box>
     )
   }
 }
 
-class TAGViz extends React.Component {
-  render() {
-    return (
-      <Box p={4} minWidth="90vw">
-        <Typography variant="h5" align="center" gutterBottom>
-          Tag
-        </Typography>
-        <Box display="flex" flexDirection="row" justifyContent="center">
-          <Box width={0.9}>
-            <TAGComponent
-              data={this.props.jsonDocument}
-              topTagCategory="chunk"
-              bottomTagCategory="POS"
-              bottomLinkCategory="universal-enhanced"
-              showTopMainLabel={true}
-              showTopArgLabels={true}
-              customParsers={[{ParserClass: CustomParser, name: 'custom'}]}
-              format="custom"
-            />
-          </Box>
-        </Box>
-      </Box>
-    )
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {jsonFile: ''}
-  }
-
-  handleTextAreaJsonInput = (json) => {
-    let json_obj = JSON.parse(json)
-    this.setState({jsonFile: json_obj})
-  }
-
-  render() {
-    let tag;
-    // only render tag if this is set
-    if (this.state.jsonFile) {
-      tag = (
-        <Grid item xs={12}>
-          <Paper>
-            <TAGViz jsonDocument={this.state.jsonFile} />
-          </Paper>
-        </Grid>
-      )
-    }
-    return (
-      <Grid container direction="column" justify="center" alignItems="center" spacing={4}>
-        <Grid item xs={12}>
-          <Paper>
-            <Box p={4} minWidth="90vw">
-              <Typography variant="h4" align="center" gutterBottom>
-                Odinson File Editor
-              </Typography>
-            </Box>
-          </Paper>
-        </Grid>
-        {tag}
-        <Grid item xs={12}>
-          <Paper>
-            <TextAreaJson onJsonDocumentChange={this.handleTextAreaJsonInput} />
-          </Paper>
-        </Grid>
-      </Grid>
-    )
-  }
-}
 
 export default App;
